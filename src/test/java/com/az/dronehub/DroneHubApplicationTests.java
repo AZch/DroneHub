825a4f5a442489ed9b5189f90a6b269ba3cd4106
AZch <antonzhuikov@gmail.com>
2023-01-15 17:37:54 +0300

    init

---
+++ src/test/java/com/az/dronehub/DroneHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.dronehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
